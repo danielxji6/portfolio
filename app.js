723602a9263df8dc5a8aaf9e5b7fc0aa2002c906
danielxji6 <danielxji6@gmail.com>
2016-03-08 14:36:16 -0800

    add all files for work

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('app.js loaded');
+
+$('#fullpage').fullpage({
+  sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+  anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+  menu: '#menu',
+  scrollingSpeed: 1000,
+  verticalCentered: false,
+  slidesNavigation: true,
+});
