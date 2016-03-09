571cad6a3de4a7d9bcf1ddf2e5fe56cf70a7f952
danielxji6 <danielxji6@gmail.com>
2016-03-08 18:44:13 -0800

    add text change function

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 console.log('app.js loaded');
 
 $('#fullpage').fullpage({
-  sectionsColor: ['whitesmoke', '#4BBFC3', '#7BAABE'],
+  sectionsColor: ['#dde9ee', '#7BAABE', '#437489'],
   anchors: ['about-page', 'project-page', 'skill-page'],
   menu: '#menu',
   scrollingSpeed: 1000,
   verticalCentered: false,
   slidesNavigation: true,
+});
+
+$('.icon').hover(function() {
+  $p = $('#contact-p');
+  var text = '';
+  switch ($(this).attr('alt')) {
+    case 'phone':
+      text = 'Give me a call 669-900-3292';
+      break;
+    case 'email':
+      text = 'Send me a email';
+      break;
+    case 'github':
+      text = 'Checkout my Github';
+      break;
+    case 'linkedin':
+      text = 'Find me on LinkedIn';
+      break;
+  }
+
+  $('#phone').click(function functionName() {
+    $('#contact-p').text('Give me a call 669-900-3292');
+  });
+
+  $p.text(text);
+  $p.css('color', 'white');
 });
